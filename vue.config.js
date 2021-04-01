b1f1959a00d1cf8151590e3a85e545c5bc0b72d7
yasiru98 <yasiru043@gmail.com>
2021-04-01 18:00:34 -0400

    addinig routing

--- vue.config.js
+++ vue.config.js
@@ -7,30 +7,5 @@ module.exports = {
       'vuetify'
     ],
 
-    pages: {
-      'index': {
-        entry: './src/pages/Splash/main.js',
-        template: 'public/index.html',
-        title: 'Splash',
-        chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
-      },
-      'login': {
-        entry: './src/pages/Login/main.js',
-        template: 'public/login.html',
-        title: 'Login',
-        chunks: [ 'chunk-vendors', 'chunk-common', 'login' ]
-      },
-      'home': {
-        entry: './src/pages/Home/main.js',
-        template: 'public/home.html',
-        title: 'Home',
-        chunks: [ 'chunk-vendors', 'chunk-common', 'home' ]
-      },
-      'addmorii': {
-        entry: './src/pages/AddMorii/main.js',
-        template: 'public/addmorii.html',
-        title: 'AddMorii',
-        chunks: [ 'chunk-vendors', 'chunk-common', 'addmorii' ]
-      },
-    }
+    
 }
