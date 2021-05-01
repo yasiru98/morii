0ccfdd83a8bfe4ade0dece955b5d746be88afa25
RITJake <jbj6116@rit.edu>
2021-05-01 08:00:26 -0400

    more sample memoriis

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify';
@@ -79,53 +78,53 @@ import picture40 from "@/assets/library/image40.jpg";
 const store = new Vuex.Store({
   state: {
     title: "",
-    story:"",
-    date:"",
-    location:"",
-    who:[],
-    photos:null,
-    videos:null,
-    songs:null,
-    moriis:[],
-    sampleMemory:false,
-    whichMorii:null,
-    
+    story: "",
+    date: "",
+    location: "",
+    who: [],
+    photos: null,
+    videos: null,
+    songs: null,
+    moriis: [],
+    sampleMemory: false,
+    whichMorii: null,
+
   },
-  getters:{
+  getters: {
 
   },
   mutations: {
-    updateTitle (state,payload) {
+    updateTitle(state, payload) {
       state.title = payload.title;
     },
-    updateStory (state,payload) {
+    updateStory(state, payload) {
       state.story = payload.story;
     },
-    updateDate (state,payload) {
+    updateDate(state, payload) {
       state.date = payload.date;
     },
-    updateLocation (state,payload) {
+    updateLocation(state, payload) {
       state.location = payload.location;
     },
-    updateWho (state,payload) {
+    updateWho(state, payload) {
       state.who = payload.who;
     },
-    updatePhotos (state,payload) {
+    updatePhotos(state, payload) {
       state.photos = payload.photos;
     },
-    updateVideos (state,payload) {
+    updateVideos(state, payload) {
       state.videos = payload.videos;
     },
-    updateSongs (state,payload) {
+    updateSongs(state, payload) {
       state.songs = payload.songs;
     },
-    updateMoriis(state,payload) {
+    updateMoriis(state, payload) {
       state.moriis.push(payload)
     },
-    updateSampleMemory(state,payload) {
+    updateSampleMemory(state, payload) {
       state.sampleMemory = payload.sampleMemory
     },
-    updateWhichMorii(state,payload) {
+    updateWhichMorii(state, payload) {
       state.whichMorii = payload.whichMorii
     },
   }
@@ -135,223 +134,227 @@ export default store;
 let newMaterial = new THREE.MeshNormalMaterial({
   flatShading: true
 });
-let newMemorii = new Memorii(
-  "The Time I Went To New Orleans + Sprained My Ankle",
-  "March 2020",
-  ["Yasiru", "Jake", "Liza", "Hannah", "Kate", "Mike"],
-  "New Orleans, LA",
-  "Laissez les bons temps rouler! In New Orleans the good times are always rolling down Bourbon Street, which has quite the party animal reputation during Mardi Gras. If you're lucky, you'll catch a second line parade strutting through the crowds. But this city isn't all Sazerac in a Solo cup. Take in the scenery of the historic French Quarter, tour the elegant Garden District, and eat your way across Nola's incredible restaurants and cafes—beignets are a must. If you love stories about things that go bump in the night, book a guided ghost tour that weaves through local taverns, alleyways, and cemeteries.",
-  "",
-  [
-    {
-      id: "1",
-      src: picture1,
-      alt: "Alt Image 1"
-    },
-    {
-      id: "2",
-      src: picture2,
-      alt: "Alt Image 2"
-    },
-    {
-      id: "3",
-      src: picture3,
-      alt: "Alt Image 3"
-    },
-    {
-      id: "4",
-      src: picture4,
-      alt: "Alt Image 4"
-    },
-    {
-      id: "5",
-      src: picture5,
-      alt: "Alt Image 5"
-    },
-    {
-      id: "6",
-      src: picture6,
-      alt: "Alt Image 6"
-    },
-    {
-      id: "7",
-      src: picture7,
-      alt: "Alt Image 7"
-    },
-    {
-      id: "8",
-      src: picture8,
-      alt: "Alt Image 8"
-    },
-    {
-      id: "9",
-      src: picture9,
-      alt: "Alt Image 9"
-    },
-    {
-      id: "10",
-      src: picture10,
-      alt: "Alt Image 10"
-    },
-    {
-      id: "11",
-      src: picture11,
-      alt: "Alt Image 11"
-    },
-    {
-      id: "12",
-      src: picture12,
-      alt: "Alt Image 12"
-    },
-    {
-      id: "13",
-      src: picture13,
-      alt: "Alt Image 9"
-    },
-    {
-      id: "14",
-      src: picture14,
-      alt: "Alt Image 10"
-    },
-    {
-      id: "15",
-      src: picture15,
-      alt: "Alt Image 11"
-    },
-    {
-      id: "16",
-      src: picture16,
-      alt: "Alt Image 12"
-    },
-    {
-      id: "17",
-      src: picture17,
-      alt: "Alt Image 1"
-    },
-    {
-      id: "18",
-      src: picture18,
-      alt: "Alt Image 2"
-    },
-    {
-      id: "19",
-      src: picture19,
-      alt: "Alt Image 3"
-    },
-    {
-      id: "20",
-      src: picture20,
-      alt: "Alt Image 4"
-    },
-    {
-      id: "21",
-      src: picture21,
-      alt: "Alt Image 5"
-    },
-    {
-      id: "22",
-      src: picture22,
-      alt: "Alt Image 6"
-    },
-    {
-      id: "23",
-      src: picture23,
-      alt: "Alt Image 7"
-    },
-    {
-      id: "24",
-      src: picture24,
-      alt: "Alt Image 8"
-    },
-    {
-      id: "25",
-      src: picture25,
-      alt: "Alt Image 9"
-    },
-    {
-      id: "26",
-      src: picture26,
-      alt: "Alt Image 10"
-    },
-    {
-      id: "27",
-      src: picture27,
-      alt: "Alt Image 11"
-    },
-    {
-      id: "28",
-      src: picture28,
-      alt: "Alt Image 12"
-    },
-    {
-      id: "29",
-      src: picture29,
-      alt: "Alt Image 9"
-    },
-    {
-      id: "30",
-      src: picture30,
-      alt: "Alt Image 10"
-    },
-    {
-      id: "31",
-      src: picture31,
-      alt: "Alt Image 11"
-    },
-    {
-      id: "32",
-      src: picture32,
-      alt: "Alt Image 12"
-    },
-    {
-      id: "33",
-      src: picture33,
-      alt: "Alt Image 1"
-    },
-    {
-      id: "34",
-      src: picture34,
-      alt: "Alt Image 2"
-    },
-    {
-      id: "35",
-      src: picture35,
-      alt: "Alt Image 3"
-    },
-    {
-      id: "36",
-      src: picture36,
-      alt: "Alt Image 3"
-    },
-    {
-      id: "37",
-      src: picture37,
-      alt: "Alt Image 4"
-    },
-    {
-      id: "38",
-      src: picture38,
-      alt: "Alt Image 4"
-    },
-    {
-      id: "39",
-      src: picture39,
-      alt: "Alt Image 5"
-    },
-    {
-      id: "40",
-      src: picture40,
-      alt: "Alt Image 6"
-    }
-  ],
-  "",
-  newMaterial
-);
+for (let i = 0; i < 10; i++) {
 
-let moriis = newMemorii;
-store.commit("updateMoriis", {
-  moriis: moriis
-});
+
+
+  let newMemorii = new Memorii(
+    "The Time I Went To New Orleans + Sprained My Ankle",
+    "March 2020",
+    ["Yasiru", "Jake", "Liza", "Hannah", "Kate", "Mike"],
+    "New Orleans, LA",
+    "Laissez les bons temps rouler! In New Orleans the good times are always rolling down Bourbon Street, which has quite the party animal reputation during Mardi Gras. If you're lucky, you'll catch a second line parade strutting through the crowds. But this city isn't all Sazerac in a Solo cup. Take in the scenery of the historic French Quarter, tour the elegant Garden District, and eat your way across Nola's incredible restaurants and cafes—beignets are a must. If you love stories about things that go bump in the night, book a guided ghost tour that weaves through local taverns, alleyways, and cemeteries.",
+    "",
+    [{
+        id: "1",
+        src: picture1,
+        alt: "Alt Image 1"
+      },
+      {
+        id: "2",
+        src: picture2,
+        alt: "Alt Image 2"
+      },
+      {
+        id: "3",
+        src: picture3,
+        alt: "Alt Image 3"
+      },
+      {
+        id: "4",
+        src: picture4,
+        alt: "Alt Image 4"
+      },
+      {
+        id: "5",
+        src: picture5,
+        alt: "Alt Image 5"
+      },
+      {
+        id: "6",
+        src: picture6,
+        alt: "Alt Image 6"
+      },
+      {
+        id: "7",
+        src: picture7,
+        alt: "Alt Image 7"
+      },
+      {
+        id: "8",
+        src: picture8,
+        alt: "Alt Image 8"
+      },
+      {
+        id: "9",
+        src: picture9,
+        alt: "Alt Image 9"
+      },
+      {
+        id: "10",
+        src: picture10,
+        alt: "Alt Image 10"
+      },
+      {
+        id: "11",
+        src: picture11,
+        alt: "Alt Image 11"
+      },
+      {
+        id: "12",
+        src: picture12,
+        alt: "Alt Image 12"
+      },
+      {
+        id: "13",
+        src: picture13,
+        alt: "Alt Image 9"
+      },
+      {
+        id: "14",
+        src: picture14,
+        alt: "Alt Image 10"
+      },
+      {
+        id: "15",
+        src: picture15,
+        alt: "Alt Image 11"
+      },
+      {
+        id: "16",
+        src: picture16,
+        alt: "Alt Image 12"
+      },
+      {
+        id: "17",
+        src: picture17,
+        alt: "Alt Image 1"
+      },
+      {
+        id: "18",
+        src: picture18,
+        alt: "Alt Image 2"
+      },
+      {
+        id: "19",
+        src: picture19,
+        alt: "Alt Image 3"
+      },
+      {
+        id: "20",
+        src: picture20,
+        alt: "Alt Image 4"
+      },
+      {
+        id: "21",
+        src: picture21,
+        alt: "Alt Image 5"
+      },
+      {
+        id: "22",
+        src: picture22,
+        alt: "Alt Image 6"
+      },
+      {
+        id: "23",
+        src: picture23,
+        alt: "Alt Image 7"
+      },
+      {
+        id: "24",
+        src: picture24,
+        alt: "Alt Image 8"
+      },
+      {
+        id: "25",
+        src: picture25,
+        alt: "Alt Image 9"
+      },
+      {
+        id: "26",
+        src: picture26,
+        alt: "Alt Image 10"
+      },
+      {
+        id: "27",
+        src: picture27,
+        alt: "Alt Image 11"
+      },
+      {
+        id: "28",
+        src: picture28,
+        alt: "Alt Image 12"
+      },
+      {
+        id: "29",
+        src: picture29,
+        alt: "Alt Image 9"
+      },
+      {
+        id: "30",
+        src: picture30,
+        alt: "Alt Image 10"
+      },
+      {
+        id: "31",
+        src: picture31,
+        alt: "Alt Image 11"
+      },
+      {
+        id: "32",
+        src: picture32,
+        alt: "Alt Image 12"
+      },
+      {
+        id: "33",
+        src: picture33,
+        alt: "Alt Image 1"
+      },
+      {
+        id: "34",
+        src: picture34,
+        alt: "Alt Image 2"
+      },
+      {
+        id: "35",
+        src: picture35,
+        alt: "Alt Image 3"
+      },
+      {
+        id: "36",
+        src: picture36,
+        alt: "Alt Image 3"
+      },
+      {
+        id: "37",
+        src: picture37,
+        alt: "Alt Image 4"
+      },
+      {
+        id: "38",
+        src: picture38,
+        alt: "Alt Image 4"
+      },
+      {
+        id: "39",
+        src: picture39,
+        alt: "Alt Image 5"
+      },
+      {
+        id: "40",
+        src: picture40,
+        alt: "Alt Image 6"
+      }
+    ],
+    "",
+    newMaterial
+  );
+  let moriis = newMemorii;
+  store.commit("updateMoriis", {
+    moriis: moriis
+  });
+
+}
 
 store.commit("updateSampleMemory", {
   sampleMemory: true
@@ -376,39 +379,47 @@ store.commit("updateSampleMemory", {
 // either be an actual component constructor created via
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
-const routes = [ 
-  { path: '/', component: Splash},
- // { path: '/login', component: Login },
-  { path: '/home', component: Home },
-  { path: '/mosaic', component: Mosaic },
-  { path: '/addmorii',
-  // You could also have named views at tho top
-  component: AddMorii,
-  children: [{
-    path: 'addmedia',
-    component: AddMedia,
-    children:[{
-      path: 'photos',
-      component: Photos,
-    },{
-      path: 'videos',
-      component: Videos,
-    },{
-      path: 'music',
-      component: Music,
-    }]
-  },{
-    path: 'tellyourstory',
-    component: TellYourStory
-  },{
-    path: 'helpusorganize',
-    component: HelpUsOrganize
-  },{
-    path: 'setthetone',
-    component: SetTheTone
+const routes = [{
+    path: '/',
+    component: Splash
   },
-]
-},
+  // { path: '/login', component: Login },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/mosaic',
+    component: Mosaic
+  },
+  {
+    path: '/addmorii',
+    // You could also have named views at tho top
+    component: AddMorii,
+    children: [{
+      path: 'addmedia',
+      component: AddMedia,
+      children: [{
+        path: 'photos',
+        component: Photos,
+      }, {
+        path: 'videos',
+        component: Videos,
+      }, {
+        path: 'music',
+        component: Music,
+      }]
+    }, {
+      path: 'tellyourstory',
+      component: TellYourStory
+    }, {
+      path: 'helpusorganize',
+      component: HelpUsOrganize
+    }, {
+      path: 'setthetone',
+      component: SetTheTone
+    }, ]
+  },
 ]
 
 // 3. Create the router instance and pass the `routes` option
@@ -422,8 +433,8 @@ const router = new VueRouter({
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
 //const app = new Vue({
- // vuetify,
- // router
+// vuetify,
+// router
 //}).$mount('#app')
 
 // Now the app has started!
@@ -437,6 +448,3 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 //import HelloWorld from '@/components/HelloWorld';
-
-
-      
